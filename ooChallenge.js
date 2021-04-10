01d3331d311362e0c24f65c4e0a8f5162704d34d
Perryvon <Perry.j.vr@gmail.com>
2021-04-10 14:13:09 -0700

    updated checker function in vehicles class, cleaned up script, completed ooChallenge

--- ooChallenge.js
+++ ooChallenge.js
@@ -1,67 +1,62 @@
 /* create a Vehicle class, with properties of make, model, and year. 
     Each vehicle should have a method attached that returns a string "Beep" & a method that returns a string outlining the make model & year */
 
-class Vehicle{
-    constructor(make, model, year){
+class Vehicle {
+    constructor(make, model, year) {
         this.make = make;
         this.model = model;
         this.year = year
     }
-    honk(){
-        return "Beep."
+    honk() {
+        return "Beep.";
     }
-    toString(){
-        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
     }
 }
 
 /* create a class for a Car which inherits from the Vehicle class but adds a set numWheels property of 4 */
-
-class Car extends Vehicle{
-    constructor(make, model, year){
+class Car extends Vehicle {
+    constructor(make, model, year) {
         super(make, model, year);
-        this.numWheels = 4
+        this.numWheels = 4;
     }
 }
 
-/* creates a class for a motorcycle which inherits from a Vehicle class, a set numWheels property of 2 and a revEngine method that returns the string "VROOM!!!" */ 
-
+/* creates a class for a motorcycle which inherits from a Vehicle class, a set numWheels property of 2 and a revEngine method that returns the string "VROOM!!!" */
 class Motorcycle extends Vehicle {
-    constructor(make, model, year){
+    constructor(make, model, year) {
         super(make, model, year);
         this.numWheels = 2;
     }
 
-    revEngine(){
-        return "VROOM!!!"
+    revEngine() {
+        return "VROOM!!!";
     }
 
 }
-
+/* create a class of Vehicles (plural) that acts as a garage. user decides limit of cars/motorcycles that can be sored*/
 class Vehicles {
-    constructor(capacity){
+    constructor(capacity) {
         this.vehicles = [];
         this.capacity = capacity;
     }
-
+    // checks to make sure vehicle being added is either a car or motorcycle.
     checkVehicle = (vehicle) => {
-        if(vehicleToAdd.constructor.name ==="Car" || vehicleToAdd.constructor.name === "Motorcycle"){
-            console.log("working with check")
+        if (vehicle.constructor.name === "Car" || vehicle.constructor.name === "Motorcycle") {
+            return true;
         } else {
-            console.log("check failed")
+            console.log("vehicles only!!!!");
+            return false;
         }
     }
-
-    add(vehicleToAdd){
-        if(vehicleToAdd.constructor.name ==="Car" || vehicleToAdd.constructor.name === "Motorcycle"){
-            if (this.vehicles.length < this.capacity){
-                this.vehicles.push(vehicleToAdd.constructor.name);
-                console.log(`${vehicleToAdd.constructor.name} added!`)
-            } else {
-                return console.log("Sorry, we’re full.");
-            }
+    // if space is available, vehicle will be added to vehicles array 
+    add(vehicleToAdd) {
+        if (this.vehicles.length < this.capacity && this.checkVehicle(vehicleToAdd)) {
+            this.vehicles.push(vehicleToAdd.constructor.name);
+            console.log(`${vehicleToAdd.constructor.name} added!`);
         } else {
-            console.log("only Vehicles can enter!")
+            return console.log("Sorry, we’re full.");
         }
     }
 }
@@ -69,15 +64,14 @@ class Vehicles {
 
 
 
+/* TESTS! */
+let firstCar = new Car("saab", "900 turbo", 1995);
+let retroBike = new Motorcycle("toyota", "roadster", 1990);
+let perrysGarage = new Vehicles(3);
 
-let carA = new Car("saab", "900 turbo", 1995)
-let bikeB = new Motorcycle("toyota","dragon",1990)
+perrysGarage.add(firstCar);
+perrysGarage.add(firstCar);
+perrysGarage.add(retroBike);
+perrysGarage.add("potato");
 
-let newGarage = new Vehicles(2);
-
-newGarage.add(carA)
-newGarage.add(bikeB)
-newGarage.add(carA)
-
-
-
+console.log(perrysGarage.vehicles);
